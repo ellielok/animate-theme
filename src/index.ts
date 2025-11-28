dd9191a8ebb23752e28dcc05d79501421192c585
ellielok <ellielo2021@gmail.com>
2025-11-29 02:01:14 +1100

    export a basic toggle theme

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 export const one = 1
 export const two = 2
+
+
+function toggleTheme() {
+  // Fallback for browsers that don't support this API:
+  if (!document.startViewTransition) {
+    toggleClassName();
+    return;
+  }
+
+  // With a View Transition:
+  document.startViewTransition(() => toggleClassName());
+}
+
+function toggleClassName() {
+  document.documentElement.classList.toggle('dark');
+}
+
+export { toggleTheme };
